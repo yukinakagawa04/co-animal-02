32baee4e4972a0b93de8e4c13ed8c9ff59b32b4e
yukinakagawa04 <9704yn@gmail.com>
2023-04-05 14:48:20 +0000

    0405-2

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,21 +1,29 @@
 <x-app-layout>
-    <div class="py-6">
-        <div class="max-w-xl mx-auto sm:px-6 lg:px-8 bg-teal-100 overflow-hidden shadow-sm sm:rounded-lg ">
-            <h1>チャンネル開設までの流れ</h1>
-            <p>お問い合わせ</p>
-            ｜
-            <p>担当者とお打ち合わせ</p>
-            ｜      
-            <p>チャンネル名などを設定して放送スタート</p>
+    <div class="mt-4">
+        <div class="max-w-xl mx-auto sm:px-6 lg:px-8  bg-teal-100 overflow-hidden shadow-sm sm:rounded-lg ">
+            <h1 class="text-3xl text-teal-400 font-bold my-1">チャンネル開設までの流れ</h1>
             
-            <a href="/service"><p>co-animalをよく知る</p></a>
+            <div class="flex items-center lg:px-8">
+              <div class="text-lg font-bold mt-4 text-slate-700">①お問い合わせ</div>
+            </div>
+            <div class="flex items-center lg:px-8">
+              <div class="text-lg font-bold mt-4 text-slate-700">②担当者とお打ち合わせ</div>
+            </div>
+            <div class="flex items-center lg:px-8">
+              <div class="text-lg font-bold mt-4 text-slate-700">③チャンネル設定して放送スタート</div>
+            </div>
+            <br>
+            <div class="mb-4 mx-auto ">
+                <a href="{{ route('service') }}" class=" mx-auto inline-block px-4 py-2 rounded-full border-2 border-teal-400 text-teal-400 bg-teal-white font-bold hover:bg-teal-400 hover:text-white">{{ __('co-animalをよく知る') }}</a>
+            </div>
         </div>
     </div>
     <!--google formをiframeで置く-->
-    <div class="py-6">
+    <div class="">
         <div class="max-w-xl mx-auto sm:px-6 lg:px-8 bg-white overflow-hidden shadow-sm sm:rounded-lg ">
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeMMGU7-kuX_w9CPukS_pE1sE8ObSv0PvK5P_CTKCIaAgY5kQ/viewform?embedded=true" width="512" height="1024" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
         </div>
     </div>
+    
     
 </x-app-layout>
--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -1,47 +1,68 @@
 <x-app-layout>
-    <div class="py-12">
+    <div class="p-12 ">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 bg-white overflow-hidden shadow-sm sm:rounded-lg ">
             <div class="object-fit">
                 <img src="{{ asset('images/logo02.png') }}" class="w-1/2 mx-auto" alt="">
             </div>
             <div class=" mx-auto text-center">
-                <h1>現地では聞けない生き物の裏話ができる<br>
-                生き物の日常を飼育員の声で届ける音声配信サービス</h1>
+                <h1 class="text-teal-400 text-3xl font-bold">現地では聞けない生き物の裏話ができる<br>生き物の日常を飼育員の声で届ける音声配信サービス</h1>
+                <br>
+                <br>
             </div>
             <div class="w-1/2 mx-auto">
-                <p>水族館の生き物ってこんなに個性的なんだ。生き物のこういうところに着目すると面白いよ。最近生まれたこんな赤ちゃんがいるよ。など、全国の飼育員の生き物好き！な気持ちが集まって、皆さんの心に癒しをお届けするプラットフォームを目指しています。生き物の魅力や楽しみ方がたくさんの人に広まれば、友人・家族など、大切な人と一緒に行きたい場所が見つかるかもしれない。推しの水族館・動物園が見つかるかもしれない。生き物と人々との関わりの選択肢をもっと広がるような活動をしていきたいと思います。</p>
+                <p class="font-bold">水族館の生き物ってこんなに個性的なんだ。生き物のこういうところに着目すると面白いよ。最近生まれたこんな赤ちゃんがいるよ。など、全国の飼育員の生き物好き！な気持ちが集まって、皆さんの心に癒しをお届けするプラットフォームを目指しています。</p>
+                <br>
+                <p class="font-bold">生き物の魅力や楽しみ方がたくさんの人に広まれば、友人・家族など、大切な人と一緒に行きたい場所が見つかるかもしれない。推しの水族館・動物園が見つかるかもしれない。生き物と人々との関わりの選択肢をもっと広がるような活動をしていきたいと思います。</p>
+                <br>
             </div>
-            <div class="object-fit">
+            <div class="object-fit border-rounded-lg">
                 <img src="{{ asset('images/lp_image05.jpeg') }}" class="w-1/2 mx-auto" alt="">
             </div>
-            <div class=" mx-auto text-center">
-                <h2>co-animalでやりたいこと</h2>
-                <div class="flex mx-auto">
-                    <div class="mx-auto">
+            <div class="mx-auto text-center max-w-3xl py-4">
+                <br>
+                <h2 class="text-teal-400 text-xl font-bold">co-animalでやりたいこと</h2>
+                <br>
+                <div class="flex justify-center">
+                    <div class="w-1/3 mx-auto">
                         <img src="{{ asset('images/icon01.png') }}" class="w-100px mx-auto" alt="">
-                        <p>生き物の様子を<br>
+                        <br>
+                        <p class="text-teal-400 font-bold">生き物の様子を<br>
                         音声配信でお届け</p>
                     </div>
-                    <div class="mx-auto">
+                    <div class="w-1/3 mx-auto">
                         <img src="{{ asset('images/icon02.png') }}" class="w-100px mx-auto" alt="">
-                        <p>推しの生き物で<br>
+                        <br>
+                        <p class="text-slate-500 font-bold">推しの生き物で<br>
                         共鳴する</p>
                     </div>
-                    <div class="mx-auto">
+                    <div class="w-1/3 mx-auto">
                         <img src="{{ asset('images/icon02.png') }}" class="w-100px mx-auto" alt="">
-                        <p>推しの生き物に<br>
+                        <br>
+                        <p class="text-slate-500 font-bold">推しの生き物に<br>
                         投げエサできる</p>
+                    </div>
+                    <br>
+                </div>
+            </div>
+            
+            <div class=>
+                <div class=" py-2 mx-auto text-center max-w-xl  mx-auto sm:px-6 lg:px-8  bg-teal-100 overflow-hidden shadow-sm sm:rounded-lg ">
+
+                    <p class="text-teal-400 text-xl font-bold">【β版公開中】生き物好きのみなさんへ</p>
+                    <br>
+                    <p class="font-bold">サービスのβ版をクローズで公開しており、サービスに関するアドバイスをしてくださるユーザーさんを募集しています。</p>
+                    <br>
+                    <div class="mb-4 mx-auto text-center">
+                        <a href="{{ route('dashboard') }}" class=" mx-auto inline-block px-4 py-2 rounded-full border-2 border-teal-400 text-teal-400 bg-teal-white font-bold hover:bg-teal-400 hover:text-white">{{ __('参加する') }}</a>
                     </div>
                 </div>
             </div>
-            <div class="max-w-3xl mx-auto sm:px-6 lg:px-8 bg-teal-100 overflow-hidden shadow-sm sm:rounded-lg w-160px">
-                <p class="mx-auto text-center">【β版公開中】生き物好きのみなさんへ</p>
-                <p class="mx-auto text-center">サービスのβ版をクローズで公開しており、サービスに関する<br>
-                アドバイスをしてくださるユーザーさんを募集しています。</p>
-            </div>
         </div>
+        
         <div>
+            <br>
             <a href=resources/views/service.blade.php><img src="{{ asset('images/banner01.png') }}" alt="" class="mx-auto"></a>
+            <br>
         </div>
     </div>
     
