c2046473f659b70b2083999c56aeb7d7d017e0f9
yukinakagawa04 <9704yn@gmail.com>
2023-04-15 08:27:34 +0000

    0415

--- resources/views/search/input.blade.php
+++ resources/views/search/input.blade.php
@@ -1,9 +1,4 @@
 
-  <x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('Search Tweet') }}
-    </h2>
-  </x-slot>
 
   <div class="py-12">
     <div class="max-w-7xl mx-auto sm:w-8/12 md:w-1/2 lg:w-5/12">
@@ -14,11 +9,11 @@
             @csrf
             <div class="flex flex-col mb-4">
               <x-input-label for="keyword" :value="__('検索')" />
-              <x-text-input id="keyword" class="block mt-1 w-full" type="text" name="keyword" :value="old('keyword')" autofocus />
+              <x-text-input id="keyword" placeholder="キーワード検索"  class="block mt-1 w-full" type="text" name="keyword" :value="old('keyword')" autofocus />
             </div>
             <div class="flex items-center justify-end mt-4">
               <x-primary-button class="ml-3">
-                {{ __('Search') }}
+                {{ __('検索する') }}
               </x-primary-button>
             </div>
           </form>
