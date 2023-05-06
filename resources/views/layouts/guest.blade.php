0effd1a252df133e15af2dd4d40b1b73bced32ad
yukinakagawa04 <9704yn@gmail.com>
2023-05-06 14:10:12 +0000

    0506-3

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <link rel="icon" type="image/png" href="{{ asset('images/logo01.png') }}">
         <title>{{ config('app.name', 'co-animal') }}</title>
 
         <!-- Fonts -->
